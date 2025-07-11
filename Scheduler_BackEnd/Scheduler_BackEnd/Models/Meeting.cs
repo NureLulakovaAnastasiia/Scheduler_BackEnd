59d068c0282fe92b830ca770bbb78df0ca6e6c95
AnastasiiaLulakova <anastasiia.lulakova@nure.ua>
2025-07-11 21:27:51 +0300

    initialize project

---
+++ Scheduler_BackEnd/Scheduler_BackEnd/Models/Meeting.cs
@@ -0,0 +1,11 @@
+﻿namespace Scheduler_BackEnd.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        public List<int> Participants { get; set; } = new List<int>();
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
---
+++ Scheduler_BackEnd/Scheduler_BackEnd/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Scheduler_BackEnd.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
